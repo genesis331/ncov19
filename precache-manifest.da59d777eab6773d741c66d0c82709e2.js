252d3025f201b86762e059e678e6e25689d58a58
genesis331 <cheah3838@gmail.com>
2020-04-13 09:45:54 +0800

    Updates

---
+++ precache-manifest.da59d777eab6773d741c66d0c82709e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e115246d5e00293e2b5f782de7b7d2b2",
+    "url": "/ncov19/index.html"
+  },
+  {
+    "revision": "ea152f73bddf13a10b0f",
+    "url": "/ncov19/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0966f9a0d27cf06ff382",
+    "url": "/ncov19/static/js/2.b057806b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ncov19/static/js/2.b057806b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea152f73bddf13a10b0f",
+    "url": "/ncov19/static/js/main.1b34bc4a.chunk.js"
+  },
+  {
+    "revision": "0a48f6335653dff52914",
+    "url": "/ncov19/static/js/runtime-main.cbd8125f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ncov19/static/media/logo.5d5d9eef.svg"
+  }
+]);
